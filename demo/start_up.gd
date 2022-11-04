15cbffd9fab013acb19f8aa5fdb8a881c01c8819
patrickdown <patrick.down@gmail.com>
2022-11-03 22:15:14 -0400

    Added enums to make gdscript in demo clearer

--- demo/start_up.gd
+++ demo/start_up.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+enum GlassesEvent {
+	E_ADDED         = 1,
+	E_LOST          = 2,
+	E_AVAILABLE     = 3,
+	E_UNAVAILABLE   = 4,
+	E_CONNECTED     = 5,
+	E_DISCONNECTED  = 6,
+	E_TRACKING      = 7,
+	E_NOT_TRACKING  = 8,
+	E_STOPPED_ON_ERROR = 9
+}
+
+
 var connected_glasses = -1
 var arvr_interface = null
 
@@ -22,18 +35,29 @@ func _ready():
 			print("service started")
 
 func on_glasses_event(glasses_num, event_num):
-	print("glasses event ", glasses_num, ", ", event_num)
-	if connected_glasses < 0 and event_num == 3:
-		print("Trying to connect ", glasses_num)
-		connected_glasses = glasses_num 
-		TiltFiveManager.connect_glasses(glasses_num, "Godot Tilt Five")
-	elif connected_glasses == glasses_num and event_num == 5:
-		print("Trying to start interface ", connected_glasses)
-		if arvr_interface.initialize():
-			print("Init okay")
-			$Viewport.arvr = true
-		else:
-			print("Failed okay")
-			TiltFiveManager.disconnect_glasses(connected_glasses)
+	print("Event on glasses# ", glasses_num, ", ", event_num)
+	if connected_glasses >= 0 and connected_glasses != glasses_num:
+		return
+	match  event_num:
+		GlassesEvent.E_AVAILABLE:
+			print("Trying to connect glasses #", glasses_num)
+			connected_glasses = glasses_num 
+			TiltFiveManager.connect_glasses(glasses_num, "Godot Tilt Five")
+		GlassesEvent.E_UNAVAILABLE:
+			connected_glasses = -1
+		GlassesEvent.E_CONNECTED:
+			print("Trying to start interface for glasses #", connected_glasses)
+			if arvr_interface.initialize():
+				print("Init okay")
+				$Viewport.arvr = true
+			else:
+				print("Failed to start interface")
+				connected_glasses = -1
+				TiltFiveManager.disconnect_glasses(connected_glasses)
+		GlassesEvent.E_TRACKING:
+			print("Started tracking glasses #",glasses_num)
+		GlassesEvent.E_NOT_TRACKING:
+			print("Stopped tracking glasses #",glasses_num)
+	
 		
 	
